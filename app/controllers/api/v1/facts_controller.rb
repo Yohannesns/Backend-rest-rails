ed85cf3a17cb8a685e7a7f2c65d94c37968f1aaa
Yohannes <Yohannesns@gmail.com>
2021-06-16 14:18:27 -0400

    work on lesson_11_rest is completed

---
+++ app/controllers/api/v1/facts_controller.rb
@@ -0,0 +1,61 @@
+  class Api::V1::FactsController < ApplicationController
+    before_action :set_fact, only: [:show, :update, :destroy]
+  
+    # GET /users/:user_id/facts
+    def index
+      @user = User.find(params[:user_id])
+      render json: @user.facts 
+    end
+  
+    # GET /users/:user_id/facts/:id
+    def show
+        @user = User.find(params[:user_id])
+        render json:@fact
+    end
+  
+    # POST /users/:user_id/facts
+    def create
+      @user = User.find(params[:user_id])
+      @fact = @user.facts.new(fact_params)
+      if @fact.save
+        render json: @fact, status: 201
+      else
+        render json: { error: 
+  "The fact entry could not be created. #{@fact.errors.full_messages.to_sentence}"},
+        status: 400
+      end
+    end
+  
+    # PUT /users/:user_id/facts/:id
+    def update
+      @fact = Fact.find(params[:id])
+      if @fact
+       @fact.update(fact_params)
+       render json: {message: 'Fact successfully updated.'}, status: 200
+      else
+       render json: {error: 'unable to update fact.'}, status: 400
+      end  
+    end
+  
+    # DELETE /users/:user_id/facts/:id
+    def destroy
+      @fact = Fact.find(params[:id])
+      if @fact
+        @fact.destroy
+        render json: {message: 'Fact successfully deleted'}, status: 200
+      else
+        render json: {error: 'unable to delete fact'}, status: 400
+      end    
+    end
+  
+    private
+  
+    def fact_params
+      params.require(:fact).permit(:fact_text, :likes)
+    end
+  
+    def set_fact
+      @fact = Fact.find(params[:id])
+    end
+  
+  end
